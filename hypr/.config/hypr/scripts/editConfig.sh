0a209e991679dcb76d4685be5867482df4363a90
Ale <ninjavfx@gmail.com>
2025-02-01 17:41:28 -0800

    new configs - added a script to edit config files via wofi

---
+++ hypr/.config/hypr/scripts/editConfig.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/bash
+#Edit hyprland config files with wofi
+
+TERM=kitty
+EDITOR=vim
+config_path="/home/ale/dotfiles/hypr/.config/hypr/configs/"
+config_files="Animation.conf\nEnvVariables.conf\nInput.conf\nKeybinds.conf\nLookFeel.conf\nSettings.conf\nStartup_Apps.conf\nWindowRules.conf\nWorkspaceRules.conf"
+
+echo "$config_path"
+option=$(echo -e "$config_files" | wofi -W 300 -H 500 -dmenu -p config:)
+
+case $option in
+$'Animation.conf')
+  $TERM -- $EDITOR "$config_path$option"
+  ;;
+$'EnvVariables.conf')
+  $TERM -- $EDITOR "$config_path$option"
+  ;;
+$'Input.conf')
+  $TERM -- $EDITOR "$config_path$option"
+  ;;
+$'Keybinds.conf')
+  $TERM -- $EDITOR "$config_path$option"
+  ;;
+$'LookFeel.conf')
+  $TERM -- $EDITOR "$config_path$option"
+  ;;
+$'Settings.conf')
+  $TERM -- $EDITOR "$config_path$option"
+  ;;
+$'Startup_Apps.conf')
+  $TERM -- $EDITOR "$config_path$option"
+  ;;
+$'WindowRules.conf')
+  $TERM -- $EDITOR "$config_path$option"
+  ;;
+$'WorkspaceRules.conf')
+  $TERM -- $EDITOR "$config_path$option"
+  ;;
+esac
